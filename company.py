30360a795e31a230cb21a077cd6ab529f1415a15
avinash166-til <54021865+avinash166-til@users.noreply.github.com>
2020-01-31 22:47:19 +0530

    Add files via upload

---
+++ company.py
@@ -0,0 +1,13 @@
+
+for people in range(1,21):
+    skill=input("enter the skill")
+    if people<=3 or people==6 or people==10 or people==14:
+        print("already reserved")
+        
+    elif skill=="python" or skill=="java" or skill=="script":
+          
+          print("you are selected")
+        
+    
+        
+    
